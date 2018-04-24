362920966111a31b307d72fbec35c618fac4a43b
X-NicON <x-icon@ya.ru>
2018-04-24 12:54:39 +0300

    update auth form

--- resources/views/auth.php
+++ resources/views/auth.php
@@ -4,14 +4,24 @@
 ?>
 
 <div class="container">
-	<form class="form-signin" method="post">
-		<div class="form-signin-heading">Вход</div>
-		<div class="form-group">
-	  	<input type="text" name="login" class="form-control" placeholder="Имя пользователя" required autofocus>
-	  </div>
-	  <div class="form-group">
-	  	<input type="password" name="pass" class="form-control" placeholder="Пароль" required>
-	  </div>
-	  <button class="btn btn-info" type="submit">Войти</button>
-	</form>
+	<div class="card form-signin">
+    <form method="post" action="">
+    	<div class="card-header bg-bluegray text-white">
+    		<h2>Авторизация</h2>
+    	</div>
+    	<div class="card-body">
+        <div class="form-group">
+        	<label for="inputEmail" class="sr-only">Email адрес или телефон</label>
+        	<input type="text" id="inputEmail" name="login" class="form-control" placeholder="Имя пользователя" required autofocus>
+        </div>
+        <div class="form-group">
+	        <label for="inputPassword" class="sr-only">Пароль</label>
+	        <input type="password" id="inputPassword" name="pass" class="form-control" placeholder="Пароль" required>
+      	</div>
+    	</div>
+    	<div class="card-footer">
+      	<button class="btn btn-outline-info" type="submit">Войти</button>
+    	</div>
+    </form>
+  </div>
 </div>
